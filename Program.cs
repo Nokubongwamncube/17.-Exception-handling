e28d757223457340d2cb516211d87ec68f602273
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-14 12:58:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter your First number");
+                double number1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter your second ");
+                double number2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Your first number divided by your second number = ");
+                Console.WriteLine(number1 / number2);
+
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Error: Enter numbers only");
+
+
+
+
+            }
+          
+
+
+            //frezze console
+            Console.ReadLine();
+
+        }
+    }
+}
